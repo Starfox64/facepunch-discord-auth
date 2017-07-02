3e67e14b66cf3f0301f9f81f3fb2ce0f0c7eb300
Starfox64 <louisdijon21@yahoo.fr>
2017-07-02 22:39:52 +0200

    Increase welcome delay from 2 to 5 sec

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ discordClient.on('guildMemberAdd', async (member) => {
 
 		setTimeout(async () => {
 			await util.sendWelcomeMessage(entryRoom, member.user, user.token);
-		}, 2000);
+		}, 5000);
 
 		return;
 	}
